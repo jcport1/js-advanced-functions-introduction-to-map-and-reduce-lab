f7389db6f7de37ed5601a40e74a5b3e924aa760e
jeffersoncuartas <jefferson.cuartas@gmail.com>
2021-02-10 15:15:46 -0500

    commit tree 85160e2623874bd8bc26292dbaca689a5fd53fb9

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Your code here
+
+function mapToNegativize(sourceArray) {
+    let newArray = sourceArray.map(x => x * -1);
+    return newArray
+}
+
+function mapToNoChange(sourceArray) {
+
+    let newArray = sourceArray.map(x => x); 
+    return newArray 
+
+}
+
+function mapToDouble(sourceArray) {
+    let newArray = sourceArray.map(x => x * 2);
+    return newArray
+}
+
+function mapToSquare(sourceArray) {
+    let newArray = sourceArray.map(x => x * x);
+    return newArray
+}
+
+function reduceToTotal(src, startingPoint=0) {
+    let total = startingPoint
+    for(let i=0; i < src.length; i++) {
+        total = total + src[i] 
+    }
+
+    return total 
+}
+
+function reduceToAllTrue(src) {
+    for(let i=0; i < src.length; i++){
+        if(!src[i]) return false 
+    }
+    return true 
+}
+
+function reduceToAnyTrue(src) {
+    for(let i=0; i < src.length; i++) {
+        if (src[i]) return true
+    }
+
+    return false 
+}
